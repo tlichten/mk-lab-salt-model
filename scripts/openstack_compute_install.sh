0f13aace5bfc1c30b041afb4d388ec1b2c4c406b
Thomas Lichtenstein <tlichten@users.noreply.github.com>
2016-11-14 16:36:06 +0100

    Update openstack_compute_install.sh

--- scripts/openstack_compute_install.sh
+++ scripts/openstack_compute_install.sh
@@ -7,4 +7,11 @@ salt "cmp*" state.apply
 # Reboot compute nodes
 echo "Rebooting compute nodes ..."
 
+
+salt 'ctl01*' cmd.run "/usr/share/contrail-utils/provision_control.py --api_server_ip 172.16.10.254 --api_server_port 8082 --host_name ctl01 --host_ip 172.16.10.101 --router_asn 64512 --admin_password workshop --admin_user admin --admin_tenant_name admin --oper add"
+salt 'ctl02*' cmd.run "/usr/share/contrail-utils/provision_control.py --api_server_ip 172.16.10.254 --api_server_port 8082 --host_name ctl02 --host_ip 172.16.10.102 --router_asn 64512 --admin_password workshop --admin_user admin --admin_tenant_name admin --oper add"
+salt 'ctl03*' cmd.run "/usr/share/contrail-utils/provision_control.py --api_server_ip 172.16.10.254 --api_server_port 8082 --host_name ctl03 --host_ip 172.16.10.103 --router_asn 64512 --admin_password workshop --admin_user admin --admin_tenant_name admin --oper add"
+
+salt 'ctl01*' cmd.run "/usr/share/contrail-utils/provision_vrouter.py --host_name cmp01 --host_ip 172.16.10.105 --api_server_ip 172.16.10.254 --oper add --admin_user admin --admin_password workshop --admin_tenant_name admin"
+
 salt "cmp*" system.reboot
